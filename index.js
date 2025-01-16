1fe94082f9339ead383169fa017baf54e56d5c5e
syko <sykolax@gmail.com>
2025-01-16 17:37:50 -0500

    feat: read image file and display the avatar

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fileDropZone = document.querySelector(".file-drop-zone");
 const fileInput = document.querySelector("#avatar-upload");
+const fileDropZoneImage = document.querySelector(".file-drop-zone img");
 
 fileDropZone.addEventListener("click", () => {
     fileInput.click();
@@ -7,11 +8,21 @@ fileDropZone.addEventListener("click", () => {
 
 fileInput.addEventListener("change", (event) => {
     avatar = event.target.files[0];
-    displayAvatar(avatar);
+    if (avatar) {
+        const fileReader = new FileReader();
+
+        fileReader.onload = (event) => {
+            displayAvatar(event.target.result);
+        }
+
+        fileReader.readAsDataURL(avatar);
+    }
 })
 
 function displayAvatar(avatar) {
-    
+    fileDropZoneImage.src = avatar;
+    fileDropZoneImage.classList.remove("upload-icon");
+    fileDropZoneImage.classList.add("avatar-uploaded-image");
     console.log(avatar);
 }
 
